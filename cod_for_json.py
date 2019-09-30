47435b4cc4a69766016adf5933e3e43920d4d140
Timur Sepety <sepety@shaolin.ru>
2019-09-30 14:41:15 +0300

    added chenges in json

--- cod_for_json.py
+++ cod_for_json.py
@@ -12,10 +12,10 @@ with open('jsonbase.json', encoding='utf-8') as date:
         for word in words:
             if len(word) <= 6:
                 continue
-
-            if word in dict:
-                dict[word] += 1
+            word_lower = word.lower()
+            if word_lower in dict:
+                dict[word_lower] += 1
             else:
-                dict[word] = 1
+                dict[word_lower] = 1
     sorted_x = sorted(dict.items(), key=operator.itemgetter(1))
     print(sorted_x[-10:])
